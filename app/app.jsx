489b618e6f178c293d59eece1cc7e5a24b6131c3
Ingvi Hermanns <ingvi.hermannsson@gmail.com>
2017-04-03 17:58:09 +0000

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,17 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Gefa Grimblinum gulrót'));
+store.dispatch(actions.setSearchText('gulrót'));
+store.dispatch(actions.toggleShowCompleted());
+
 // Load foundation
 $(document).foundation();
 
