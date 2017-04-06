0a5c0627ac67390cc1c4b2f2381c61a28218b080
Ingvi Hermanns <ingvi.hermannsson@gmail.com>
2017-04-06 15:06:11 +0000

    Move firebase credentials to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 try {
   var config = {
-      apiKey: "AIzaSyAtpWwhLynSaaDvZetJSbHmLMuQUECgxAs",
-      authDomain: "ingvi-todo-app.firebaseapp.com",
-      databaseURL: "https://ingvi-todo-app.firebaseio.com",
-      projectId: "ingvi-todo-app",
-      storageBucket: "ingvi-todo-app.appspot.com",
-      messagingSenderId: "182315805360"
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
   firebase.initializeApp(config);
 } catch (e) {
